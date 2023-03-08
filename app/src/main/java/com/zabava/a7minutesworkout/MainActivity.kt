d15a198f3a7d0f7a138cd3e74c7a78a1792b182e
Zabi <zbvmihai@gmail.com>
2023-03-08 17:36:09 +0000

    add viewBinding as build feature to gradle

--- app/src/main/java/com/zabava/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/zabava/a7minutesworkout/MainActivity.kt
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.zabava.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
+        binding?.flStart?.setOnClickListener{
             Toast.makeText(this,"Here will start the exercise",Toast.LENGTH_SHORT).show()
         }
+
     }
 }
