8a1c81e6874b47291639674d6130018bfefda7e9
Zabi <zbvmihai@gmail.com>
2023-03-08 17:27:15 +0000

    add UI design

--- app/src/main/java/com/zabava/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/zabava/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.zabava.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener{
+            Toast.makeText(this,"Here will start the exercise",Toast.LENGTH_SHORT).show()
+        }
     }
 }
